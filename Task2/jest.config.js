78177adc3c7252055d52e2fc50f3f149dac9390c
Adnan Haider <adnanhaider@Adnans-MacBook-Pro.local>
2022-10-30 15:32:16 +0500

    jest support added

---
+++ Task2/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    preset: 'react-native',
+    transform: {
+      "^.+\\.(js|jsx)$": "babel-jest",
+    },
+    transformIgnorePatterns: [
+        'node_modules/(?!@react-native|react-native)'
+      ],
+  };
--- Task2/src/components/UserComponent.js
+++ Task2/src/components/UserComponent.js
@@ -5,8 +5,8 @@ const UserComponent = ({name, age, count}) => {
   return (
     <View style={styles.container}>
     <View style={styles.sub_container}>
-    <Text style={styles.text_style}>Name: {name}</Text>
-    <Text style={styles.text_style}>Age: {age}</Text>
+    <Text style={styles.text_style}>Name: {name ? name: 'Nill'}</Text>
+    <Text style={styles.text_style}>Age: {age ? age: 'Nill'}</Text>
     </View>
     <Text style={styles.text_style}>Count: {count} </Text>
   </View>
@@ -17,7 +17,18 @@ export {UserComponent};
 
 const styles = StyleSheet.create({
     container: {
-        width:'90%',minHeight:30,alignItems:'center', backgroundColor:'#D3D3D3',alignSelf:'center',margin:10,padding:8,borderRadius:10,
+        width:'90%',minHeight:30,alignItems:'center', backgroundColor:'white',alignSelf:'center',margin:10,padding:8,borderRadius:10,
+        shadowColor: "#000",
+shadowOffset: {
+	width: 0,
+	height: 2,
+},
+shadowOpacity: 0.25,
+shadowRadius: 3.84,
+
+elevation: 5,
+borderWidth:0.5,
+borderColor: '#D3D3D3'
     },
     sub_container: {
         width:'90%',flexDirection:'row',justifyContent:'space-around'
